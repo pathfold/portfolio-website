53f8824c9f1b01271135dd5940bd2f87961594f6
pathfold <pathfolder@gmail.com>
2021-10-31 17:23:29 -0400

    aos animations added, header <a> animations added, title updated

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,20 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./Landing.scss";
 import headImg from "../images/headImg.jpg";
+import Aos from "aos";
+import "aos/dist/aos.css";
 
-export default class Landing extends React.Component {
+export default class Header extends React.Component {
+    componentDidMount() {
+        Aos.init();
+        this.props.updateTitle("Justin Kwong")
+    }
+
     render() {
         return (
             <div class="hero">
                 <div class="hero-margin">
                     <div class="hero-left">
-                        <h2 class="hero-text-top">Hi, I'm</h2>
-                        <h1 class="hero-text-bottom">Justin Kwong</h1>
-                        <div class="hero-text-divider divider"></div>
-                        <a class="hero-text-btn btn" href="#header">See My Projects</a>
+                        <h2 class="hero-text-top" data-aos="fade-up" data-aos-duration="1500">Hi, I'm</h2>
+                        <h1 class="hero-text-bottom" data-aos="fade-up" data-aos-delay="150" data-aos-duration="1500">Justin Kwong</h1>
+                        <div class="hero-text-divider divider" data-aos="fade-up" data-aos-delay="300" data-aos-duration="1500"></div>
+                        <div data-aos="fade-up" data-aos-delay="450" data-aos-duration="1500">
+                            <a class="hero-text-btn btn" href="#header">See My Projects</a>
+                        </div>
                     </div>
                     <div class="hero-right">
-                        <img class="hero-head-img" src={headImg}></img>
+                        <img class="hero-head-img" src={headImg} data-aos="fade-up" data-aos-delay="450" data-aos-duration="1500"></img>
                     </div>
                 </div>
             </div>
