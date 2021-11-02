e40b2dc455815b549b43df238853567abcad69ef
pathfold <pathfolder@gmail.com>
2021-11-02 13:20:04 -0400

    hid header-nav for now until responsive

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,13 +9,15 @@ export default class Header extends React.Component {
                     <div class="nav-logo">
                         <a class="nav-logo-text" href="/">JK</a>
                     </div>
-                    <ul class="nav-list">
-                        <li class="nav-item"><a class="nav-link" href="#projects">Projects</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#aboutme">About Me</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#resume">Resume</a></li>
-                    </ul>
+                    <ul class="nav-list"></ul>
                 </div>
             </header>
         );
     }
 }
+
+/*
+<li class="nav-item"><a class="nav-link" href="#projects">Projects</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#aboutme">About Me</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#resume">Resume</a></li>
+*/
